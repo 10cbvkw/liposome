b04e8d757913cbb81f0bda63f50fbc17776c5866
EnzeYe <56290518+10cbvkw@users.noreply.github.com>
2024-05-08 15:19:16 +0800

    Add files via upload

---
+++ save_jpg.py
@@ -0,0 +1,30 @@
+import mrcfile
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+def mrc_to_jpg(mrc_path, jpg_path):
+    # Open the MRC file
+    with mrcfile.open(mrc_path, permissive=True) as mrc:
+        img_data = mrc.data
+
+    # Normalize the image data to scale from 0 to 255
+    normalized_img = (img_data - np.min(img_data)) / (np.max(img_data) - np.min(img_data))
+    image_8bit = (normalized_img * 255).astype(np.uint8)
+
+    # Create an image and save as JPEG
+    image = Image.fromarray(image_8bit)
+    image.save(jpg_path)
+    print(f"Image saved as {jpg_path}")
+
+    # Optionally display the image
+    # plt.imshow(image_8bit, cmap='gray')
+    # plt.title('CryoEM Image')
+    # plt.axis('off')  # Turn off axis numbers and ticks
+    # plt.show()
+
+# Example usage
+for i in range(1, 10):
+    mrc_path = '/home/pc/Desktop/liposome/stack_2nd_010'+str(i)+'_DW.mrc'  # Path to your MRC file
+    jpg_path = '/home/pc/Desktop/liposome/images/stack_2nd_010'+str(i)+'_DW.jpg'  # Desired output path for the JPEG file
+    mrc_to_jpg(mrc_path, jpg_path)
