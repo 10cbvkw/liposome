e540a620d10800f3ca241d32f3dba7e538042d01
EnzeYe <56290518+10cbvkw@users.noreply.github.com>
2024-08-16 16:03:29 +0800

    Add files via upload
    debug for original matlab code

---
+++ getStackImagePixelsInMicrograph_new.m
@@ -0,0 +1,136 @@
+%GETSTACKIMAGEPIXELSINMICROGRAPH Function that interpolates vesicle image
+%pixels from the micrograph into normalised (meshgrid) form, given the
+%vesicle position and radius
+
+function [mImagePixelsInMicExtForInterp, XabsForInterp, YabsForInterp, mImagePixelsInMicExtForInterpWin, mImagePixelsInMicExtForInterpBGWin, vImagePixelsCropInfo, mVesXrelR, mVesYrelA, mImagePixelsInMicExtForInterpHardWin] = getStackImagePixelsInMicrograph(mMicrograph, mXabsVes, mYabsVes, bCreateWindows, dRadiusVes, dBeta, dAlpha, mMicrographBinaryVesicles, bAddHardVesWin, bCalcCropInfo, dMaxRR, dXVes, dYVes, iVesShapeInterpMethod, dWallThickness)
+
+    mImagePixelsInMicExtForInterp = [];
+    mImagePixelsInMicExtForInterpWin = [];
+    mImagePixelsInMicExtForInterpBGWin = [];
+    XabsForInterp = [];
+    YabsForInterp = [];
+    mImagePixelsInMicExtForInterpHardWin = [];
+    vImagePixelsCropInfo = [];
+    mVesXrelR = []; 
+    mVesYrelA = [];
+        
+    
+    XcntVal = mXabsVes(1, floor(length(mXabsVes(1,:))/2) + 1);
+    Xcnt = floor(XcntVal); % The relevant pixel value
+
+    YcntVal = mYabsVes(floor(length(mYabsVes(:,1))/2) + 1, 1);
+    Ycnt = floor(YcntVal); % The relevant pixel value
+
+    offset = max(Xcnt - floor(min(mXabsVes(:))), Ycnt - floor(min(mYabsVes(:))));
+    
+    if nargin > 9 && bCalcCropInfo
+        
+        [Xrel, Yrel, dummy, XrelR, YrelA] = createVesInterpRelativeMeshGrids('CAR', dRadiusVes, dMaxRR);
+        offset = floor(size(XrelR,1)/2) + 1;
+        
+        if size(XrelR,1) > (2*offset)
+            error('Invalid image format');
+        end
+    else
+        offset = offset + round(offset/10);
+    end
+    
+
+    if (Ycnt-offset >= 1) && (Ycnt+offset <= size(mMicrograph, 1)) && (Xcnt-offset >= 1) && (Xcnt+offset <= size(mMicrograph, 2))
+
+        mImBoxX = [Xcnt-offset; Xcnt+offset];
+        mImBoxY = [Ycnt-offset; Ycnt+offset];
+        mImagePixelsInMicExtForInterp = mMicrograph(mImBoxY(1):mImBoxY(2), mImBoxX(1):mImBoxX(2));
+
+        newCnt = ceil(size(mImagePixelsInMicExtForInterp, 1)/2);
+        if mMicrograph(Ycnt, Xcnt) ~= mImagePixelsInMicExtForInterp(newCnt, newCnt)
+            error('Pixel centering error');
+        end
+
+        XabsForInterp = mXabsVes - (Xcnt - newCnt);
+        YabsForInterp = mYabsVes - (Ycnt - newCnt);
+        
+
+        if nargin > 9 && bCalcCropInfo
+                    
+            
+            
+            vImagePixelsCropInfo = zeros(8,1);
+            
+            dRadiusDestVes = dRadiusVes;
+            dRadiusOrigVes = dRadiusVes;
+            dXOrigVes = dXVes;
+            dYOrigVes = dYVes;
+            
+            
+            [Xabs, Yabs] = createVesInterpAbsoluteMeshGrids(dRadiusDestVes, XrelR, YrelA, dXOrigVes, dYOrigVes, dRadiusOrigVes, iVesShapeInterpMethod, dWallThickness);
+            Xabs = reshape(Xabs, size(XrelR,1), size(XrelR,2));
+            Yabs = reshape(Yabs, size(XrelR,1), size(XrelR,2));
+                        
+            if length(floor(min(Yabs(:))):ceil(max(Yabs(:)))) ~= length(floor(min(Xabs(:))):ceil(max(Xabs(:))))
+                error('Invalid image dimensions!');
+            end
+            
+            vImagePixelsCropInfo(5:8) = [round(min(Yabs(:))), round(max(Yabs(:))), round(min(Xabs(:))), round(max(Xabs(:)))];
+            mImagePixelsInMic = mMicrograph(vImagePixelsCropInfo(5):vImagePixelsCropInfo(6), vImagePixelsCropInfo(7):vImagePixelsCropInfo(8));
+            if size(mImagePixelsInMic,1) ~= size(Xabs,1)
+                error('Invalid image dimensions!');
+            end
+            
+            
+            % Now we take the pixels out of mImagePixelsInMicExtForInterp
+            dXOrigVes = dXVes - (Xcnt - newCnt);
+            dYOrigVes = dYVes - (Ycnt - newCnt);
+
+            [Xabs, Yabs] = createVesInterpAbsoluteMeshGrids(dRadiusDestVes, XrelR, YrelA, dXOrigVes, dYOrigVes, dRadiusOrigVes, iVesShapeInterpMethod, dWallThickness);
+            Xabs = reshape(Xabs, size(XrelR,1), size(XrelR,2));
+            Yabs = reshape(Yabs, size(XrelR,1), size(XrelR,2));
+            
+            
+            vImagePixelsCropInfo(1:4) = [round(min(Yabs(:))), round(max(Yabs(:))), round(min(Xabs(:))), round(max(Xabs(:)))];
+            mImagePixelsInMic2 = mImagePixelsInMicExtForInterp(vImagePixelsCropInfo(1):vImagePixelsCropInfo(2), vImagePixelsCropInfo(3):vImagePixelsCropInfo(4));
+            
+            % FIXME
+            if size(mImagePixelsInMic) ~= size(mImagePixelsInMic2)
+                mImagePixelsInMicExtForInterp = [];
+                XabsForInterp = [];
+                YabsForInterp = [];
+                mImagePixelsInMicExtForInterpWin = [];
+                mImagePixelsInMicExtForInterpBGWin = [];
+                vImagePixelsCropInfo = [];
+                mVesXrelR = [];
+                mVesYrelA = [];
+                mImagePixelsInMicExtForInterpHardWin = [];
+                return;
+            end
+
+            if sum(mImagePixelsInMic(:)-mImagePixelsInMic2(:)) ~= 0
+                error('Invalid image dimensions!');
+            end
+            
+            % return new values
+            mVesXrelR = XrelR;
+            mVesYrelA = YrelA;
+        end
+          
+        
+        if bCreateWindows
+
+            ptVesicleCnt = [YabsForInterp(floor(length(YabsForInterp(:,1))/2) + 1, 1), XabsForInterp(1, floor(length(XabsForInterp(1,:))/2) + 1)];
+            mImagePixelsInMicExtForInterpWin = createVesicleImageSignalWindow(size(mImagePixelsInMicExtForInterp), dRadiusVes, ptVesicleCnt, dBeta, dAlpha, 1);
+
+            if nargin > 8 && bAddHardVesWin
+                mImagePixelsInMicExtForInterpHardWin = createVesicleImageSignalWindow(size(mImagePixelsInMicExtForInterp), dRadiusVes, ptVesicleCnt, dBeta, dAlpha, 0);
+            end
+            
+            if size(mMicrographBinaryVesicles,1) > 0
+
+                mImagePixelsInMicExtForInterpBGWin = mMicrographBinaryVesicles(Ycnt-offset:Ycnt+offset, Xcnt-offset:Xcnt+offset);
+                
+            end
+
+        end
+
+    end
+
+end    
---
+++ removeVesiclesFromMicrograph_new.m
@@ -0,0 +1,89 @@
+
+function removeVesiclesFromMicrograph(stVesicleModel, iPrincCompReg, iPrincCompRad, iPrincCompAng, mMicrograph, vVesicleCntX, vVesicleCntY, vVesicleR, bShowFigure, sTestMicText)
+
+
+    iNrOfVesicles = length(vVesicleR);
+    
+    mMicrographBinaryVesicles = createBinaryVesicleMicrograph(size(mMicrograph), vVesicleCntX, vVesicleCntY, vVesicleR, stVesicleModel.stParameters.dBetaBGWin);
+    mMicrographBinaryVesicles(:) = ~mMicrographBinaryVesicles(:);
+    
+    % Cartesian coordinates
+
+    [Xabs, Yabs] = createVesInterpAbsoluteMeshGridsForMic('CAR', vVesicleCntX, vVesicleCntY, vVesicleR, stVesicleModel.stParameters, mMicrographBinaryVesicles);
+    
+    % For interpolating polar coordinates (iVesShapeInterpMethod == 3)
+    if stVesicleModel.stParameters.iVesShapeInterpMethod == 2
+        [Rabs, Aabs, mVesicleImWinStackInterpPOL] = createVesInterpAbsoluteMeshGridsForMic('POL', vVesicleCntX, vVesicleCntY, vVesicleR, stVesicleModel.stParameters, mMicrographBinaryVesicles);
+    end
+    
+    mMicrographVesiclesSubtracted = mMicrograph;
+    vesicleIdxRemoved = zeros(iNrOfVesicles,1);
+
+    for vesIdx = 1:iNrOfVesicles
+
+        % Correction of X and Y values to boxed image: Center
+        % coordinates should be aligned
+        
+        [mImagePixelsInMicrograph, mXvesIm, mYvesIm, mImagePixelsVesicleWin, mImagePixelsBGWin, vImagePixelsCropInfo, mVesXrelR, mVesYrelA, mImagePixelsVesicleHardWin] = getStackImagePixelsInMicrograph(mMicrographVesiclesSubtracted, Xabs(:, :, vesIdx), Yabs(:, :, vesIdx), 1, vVesicleR(vesIdx), stVesicleModel.stParameters.dBeta, stVesicleModel.stParameters.dAlpha, mMicrographBinaryVesicles, 1, 1, stVesicleModel.stParameters.dMaxRR, vVesicleCntX(vesIdx), vVesicleCntY(vesIdx), stVesicleModel.stParameters.iVesShapeInterpMethod, stVesicleModel.stParameters.dWallThickness);
+        if ~isempty(mImagePixelsInMicrograph)
+            if isValidVesicleImage(mImagePixelsInMicrograph) && isValidVesicleBGImage(mImagePixelsBGWin) 
+    
+    
+                [mImagePixelsInMicrographNorm, dImageMean, dStd] = normalizeVesicleImage(mImagePixelsInMicrograph, mImagePixelsVesicleWin, mImagePixelsBGWin, 0);
+    
+                mImNormInterp = interpolateImagePixels(mImagePixelsInMicrographNorm, mXvesIm, mYvesIm);
+                
+                mImNormInterp(isnan(mImNormInterp(:)))=0;
+                
+                mImNormInterpPOLWindowed = [];
+                if stVesicleModel.stParameters.iVesShapeInterpMethod == 2
+                    mImNormInterpPOL = interpolateImagePixels(mMicrographVesiclesSubtracted, Rabs(:, :, vesIdx), Aabs(:, :, vesIdx));
+                    mImNormInterpPOLWindowed = mImNormInterpPOL.*mVesicleImWinStackInterpPOL(:,:,vesIdx);
+    
+                end
+    
+    
+                if isValidVesicleBGImage(mImagePixelsBGWin(vImagePixelsCropInfo(1):vImagePixelsCropInfo(2), vImagePixelsCropInfo(3):vImagePixelsCropInfo(4)))
+                
+    
+                    
+                    mImBoxSub = removeVesicleFromImage(stVesicleModel,... 
+                        iPrincCompReg,...
+                        mImagePixelsInMicrograph(vImagePixelsCropInfo(1):vImagePixelsCropInfo(2),vImagePixelsCropInfo(3):vImagePixelsCropInfo(4)),...
+                        mImagePixelsVesicleWin(vImagePixelsCropInfo(1):vImagePixelsCropInfo(2),vImagePixelsCropInfo(3):vImagePixelsCropInfo(4)),...
+                        mImagePixelsVesicleHardWin(vImagePixelsCropInfo(1):vImagePixelsCropInfo(2),vImagePixelsCropInfo(3):vImagePixelsCropInfo(4)),...
+                        mImagePixelsBGWin(vImagePixelsCropInfo(1):vImagePixelsCropInfo(2),vImagePixelsCropInfo(3):vImagePixelsCropInfo(4)),...
+                        mImagePixelsInMicrographNorm(vImagePixelsCropInfo(1):vImagePixelsCropInfo(2),vImagePixelsCropInfo(3):vImagePixelsCropInfo(4)),...
+                        mImNormInterpPOLWindowed, mVesXrelR, mVesYrelA, vVesicleR(vesIdx), dImageMean, dStd); 
+                        
+                    
+                    
+    %                 if bShowFigure && sum(ismember(stVesicleModel.stParameters.showFigIdx, vesIdx)) 
+    %         
+    %                     fprintf( 'std %d \n', std(mImBoxSub(:))); % TODO remove
+    %                 
+    %                 end
+                    
+                    
+                    if isValidVesicleImage(mImBoxSub)
+    
+                        
+                        mMicrographVesiclesSubtracted(vImagePixelsCropInfo(5):vImagePixelsCropInfo(6),vImagePixelsCropInfo(7):vImagePixelsCropInfo(8)) = mImBoxSub;
+                        vesicleIdxRemoved(vesIdx) = 1;
+    
+                        
+                    end
+                end
+            end
+        else
+            continue
+        end
+    end
+    
+    clear mMicrographBinaryVesicles;
+        
+    if stVesicleModel.stParameters.bSaveData
+        % Save vesicle subtracted micrograph
+        saveVesicleSubtractedMicrograph(stVesicleModel, mMicrograph, mMicrographVesiclesSubtracted, strcat(stVesicleModel.sName, '-mic-', sTestMicText), vVesicleCntX, vVesicleCntY, vVesicleR, vesicleIdxRemoved, iPrincCompReg, iPrincCompRad, iPrincCompAng, sTestMicText);
+    end
+end
